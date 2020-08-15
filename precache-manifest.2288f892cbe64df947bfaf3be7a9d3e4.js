3be65e7971dc8e3dd5d40baaf6e3bc844e5e744f
Andrew Zucker <wisemantis@Mantis-iMac.local>
2020-08-15 00:50:04 -0400

    Updates

---
+++ precache-manifest.2288f892cbe64df947bfaf3be7a9d3e4.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "92c1b35bed0e0312f98869ef915a432e",
+    "url": "/react_todo_pages/index.html"
+  },
+  {
+    "revision": "61bbaf50658d81708f87",
+    "url": "/react_todo_pages/static/css/main.373b6259.chunk.css"
+  },
+  {
+    "revision": "84a359460a297d419659",
+    "url": "/react_todo_pages/static/js/2.26bebd8b.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/react_todo_pages/static/js/2.26bebd8b.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "61bbaf50658d81708f87",
+    "url": "/react_todo_pages/static/js/main.bedaa957.chunk.js"
+  },
+  {
+    "revision": "2d09d89d7a29fdc00db3",
+    "url": "/react_todo_pages/static/js/runtime-main.7a402030.js"
+  }
+]);
